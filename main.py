a935462dc0a4acf6a47e1fa399dc3363290072b4
jakubsen855-a11y <jakubsen855@gmail.com>
2026-06-29 18:09:34 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
 import discord
-from discord import app_commands
 from discord.ext import commands
 
 from kolo import setup_kolo
@@ -12,13 +11,13 @@ intents.members = True
 intents.voice_states = True
 intents.message_content = True
 
-bot = commands.Bot(command_prefix=!, intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 setup_kolo(bot)
 
 @bot.event
-async def on_ready()
+async def on_ready():
     await bot.tree.sync()
-    print(fPřihlášen jako {bot.user})
+    print(f"Přihlášen jako {bot.user}")
 
 bot.run(TOKEN)
